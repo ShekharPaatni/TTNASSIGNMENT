f4f4fc720a1138284e322e61e3944c6835fa768a
ShekharPaatni <chandrashekhar.paatni@tothenew.com>
2017-02-23 14:58:30 +0530

    second assisment

--- AdvanceJavaScript/exercise9.js
+++ AdvanceJavaScript/exercise9.js
@@ -1,5 +1,25 @@
 function check(){
 var str=document.getElementById("texty").value;
+var max=[];
 
+
+for(var i=0;i<str.length;i++){
+
+if(str[i] in max)
+{
+  max[str[i]]++;
+}else{
+  max[str[i]]=1;
+}
+}
+var maximum=0;
+var val=0;
+for(var i in max){
+    if(maximum<max[i]){
+      maximum=max[i];
+      val=i;
+    }
+  }
+console.log(val);
 
 }
